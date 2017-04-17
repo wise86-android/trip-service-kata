2128a766ae07d291a3abf7bc9178ebf5167cf9fd
Wise86 <wise86android@gmail.com>
2017-04-17 14:17:11 +0200

    add test eviroment

---
+++ c++/test/compileTest.cpp
@@ -0,0 +1,27 @@
+//
+// Created by wise on 4/17/17.
+//
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include "TripServiceSupport.h"
+
+TEST(compileTest, createExecWithAllTheDependency){
+
+    try {
+        User temp(0);
+        TripService::GetTripsByUser(temp);
+    }catch (...){
+
+    }
+    SUCCEED();
+}
+
+
+TEST(UserSession,IsSingleton){
+
+    auto *session = UserSession::GetInstance();
+
+    ASSERT_EQ(session,UserSession::GetInstance());
+}
